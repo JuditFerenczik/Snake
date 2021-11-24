c8b6d0cdd544a9ee62f464d45cac68797cb01267
Judit Ferenczik <jferenczik@gmail.com>
2021-11-24 18:06:38 +0100

    addition of the missing classes

---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+ALLIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("red")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score:  {self.score}",align=ALLIGNMENT,font=FONT)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score:  {self.score}", align=ALLIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALLIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
--- snake.py
+++ snake.py
@@ -6,24 +6,28 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
-
 class Snake:
 
     def __init__(self):
-        self.segments = []
+        self.segments =[]
         self.create_snake()
         self.head = self.segments[0]
 
     def create_snake(self):
-        for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+        for position in STARTING_POSITIONS:   
+            self.add_segment(position)
+    def add_segment(self, position):
+        segment = Turtle("square")
+        segment.color("white")
+        segment.penup()
+        segment.goto(position)
+        self.segments.append(segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1):
+        for seg_num in range(len(self.segments)-1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
@@ -34,11 +38,11 @@ class Snake:
             self.head.setheading(UP)
 
     def down(self):
-        if self.head.heading() != UP:
+        if self.head.heading() !=  UP:
             self.head.setheading(DOWN)
 
     def left(self):
-        if self.head.heading() != RIGHT:
+        if self.head.heading() !=  RIGHT:
             self.head.setheading(LEFT)
 
     def right(self):
